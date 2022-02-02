2750d2e63be07ad9b3b0bd3ac888c6557bc26001
pmanecci <pmanecci@gmail.com>
2022-02-01 19:55:50 -0500

    Added delete function and route and refactored code

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,17 +1,35 @@
 const router = require('express').Router();
-const { createNewNote } = require('../../lib/notes');
-const { notes } = require('../../db/db.json');
+const { filterByQuery, findById, createNewNote, deleteNote } = require('../../lib/notes');
+let notes = require('../../db/db.json').notes;
 
 
 router.get('/notes', (req, res) => {
   let results = notes;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
   res.json(results);
 });
 
+router.get('/notes/:id', (req, res) => {
+  const result = findById(req.params.id, notes);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
 router.post('/notes', (req, res) => {
-  // set id based on what the next index of the array will be
     const note = createNewNote(req.body, notes);
     res.json(note);
 });
+
+router.delete('/notes/:id', (req, res) => {
+  const notesArray = deleteNote(req.params.id, notes);
+  delete require.cache[require.resolve('../../db/db')];
+  notes = require('../../db/db').notes;
+  res.json(notesArray);
+})
 
 module.exports  = router;
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
-
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,5 +14,5 @@ app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server now on port 3001!`);
+    console.log(`API server now on port ${PORT}!`);
 });
