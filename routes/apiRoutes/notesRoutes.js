dd45e4eb75571e2f9350ae06330f8a162a875786
pmanecci <pmanecci@gmail.com>
2022-02-01 19:26:45 -0500

    created library for notes js functions

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,17 +1,7 @@
 const router = require('express').Router();
+const { createNewNote } = require('../../lib/notes');
 const { notes } = require('../../db/db.json');
 
-const generateUniqueId = require('generate-unique-id');
-
-function createNewNote(body, notesArray) {
-    const note = body;
-    notesArray.push(note);
-    fs.writeFileSync(
-        path.join(__dirname, '../../db/db.json'),
-        JSON.stringify({ notes: notesArray }, null, 2)
-    );
-    return note;
-}; 
 
 router.get('/notes', (req, res) => {
   let results = notes;
@@ -20,7 +10,6 @@ router.get('/notes', (req, res) => {
 
 router.post('/notes', (req, res) => {
   // set id based on what the next index of the array will be
-    req.body.id = generateUniqueId();
     const note = createNewNote(req.body, notes);
     res.json(note);
 });
--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-app.use('api', apiRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port 3001!`);
